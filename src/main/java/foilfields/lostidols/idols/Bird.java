297b22a4a3db8fd51aee54064e2a55e8ec14a16d
Lucas Eldon <lucas.eldon.2021@live.rhul.ac.uk>
2023-11-18 15:21:55 +0000

    Bird charge conditions done

--- src/main/java/foilfields/lostidols/idols/Bird.java
+++ src/main/java/foilfields/lostidols/idols/Bird.java
@@ -1,23 +1,70 @@
 package foilfields.lostidols.idols;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ShapeContext;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.block.*;
+import net.minecraft.client.util.ParticleUtil;
+import net.minecraft.entity.FallingBlockEntity;
+import net.minecraft.particle.BlockStateParticleEffect;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 
-public class Bird extends AbstractIdol {
+public class Bird extends AbstractIdol implements LandingBlock {
     public Bird(Settings settings) {
         super(settings);
+    }
+
+    public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
+        world.scheduleBlockTick(pos, this, 2);
+    }
+
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        world.scheduleBlockTick(pos, this, 2);
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
+
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (canFallThrough(world.getBlockState(pos.down())) && pos.getY() >= world.getBottomY()) {
+            FallingBlockEntity.spawnFromBlock(world, pos, state).setHurtEntities(2.0F, 40);
+        }
+    }
+
+    public static boolean canFallThrough(BlockState state) {
+        return state.isAir() || state.isIn(BlockTags.FIRE) || state.isLiquid() || state.isReplaceable();
+    }
+
+    public void onLanding(World world, BlockPos pos, BlockState fallingBlockState, BlockState currentStateInPos, FallingBlockEntity fallingBlockEntity) {
+        if (fallingBlockEntity.timeFalling > 70 && !fallingBlockState.get(CHARGED)) {
+            world.setBlockState(pos, fallingBlockState.cycle(CHARGED), Block.NOTIFY_LISTENERS);
+        }
+
+        if (!fallingBlockEntity.isSilent()) {
+            world.syncWorldEvent(1031, pos, 0);
+        }
+    }
+
+    public void onDestroyedOnLanding(World world, BlockPos pos, FallingBlockEntity fallingBlockEntity) {
+        if (!fallingBlockEntity.isSilent()) {
+            world.syncWorldEvent(1029, pos, 0);
+        }
+    }
+
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        if (random.nextInt(16) == 0) {
+            BlockPos blockPos = pos.down();
+            if (canFallThrough(world.getBlockState(blockPos))) {
+                ParticleUtil.spawnParticle(world, pos, random, new BlockStateParticleEffect(ParticleTypes.BLOCK, state));
+            }
+        }
     }
 
     @Override
@@ -28,14 +75,5 @@ public class Bird extends AbstractIdol {
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
         return VoxelShapes.cuboid(0.3f, 0f, 0.3f, 0.7f, 1.0f, 0.7f);
-    }
-
-    @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (!world.isClient) {
-            world.setBlockState(pos, state.cycle(CHARGED), Block.NOTIFY_LISTENERS);
-        }
-
-        return ActionResult.SUCCESS;
     }
 }
