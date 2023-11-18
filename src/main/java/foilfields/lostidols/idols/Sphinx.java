759f7b2f29221d53312f1983e65e6dad88646d15
Jamie Markiewicz <jmarkiewicz88@outlook.com>
2023-11-18 16:17:00 +0000

    Update Sphinx.java

--- src/main/java/foilfields/lostidols/idols/Sphinx.java
+++ src/main/java/foilfields/lostidols/idols/Sphinx.java
@@ -3,17 +3,36 @@ package foilfields.lostidols.idols;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageSources;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleType;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import org.joml.Vector3d;
+
+import java.util.List;
 
 public class Sphinx extends AbstractIdol {
     public Sphinx(Settings settings) {
@@ -22,7 +41,18 @@ public class Sphinx extends AbstractIdol {
 
     @Override
     public void tick(BlockState state, World world, BlockPos position) {
-        if (state.get(CHARGED)) world.playSound(null, position, SoundEvents.ENTITY_CAT_PURREOW, SoundCategory.BLOCKS);
+        if (state.get(CHARGED)) {
+            //world.playSound(null, position, SoundEvents.ENTITY_CAT_PURREOW, SoundCategory.BLOCKS);
+            Box area = new Box(new Vec3d(position.getX() - 50, position.getY() - 50, position.getZ() - 50), new Vec3d(position.getX() + 50, position.getY() + 50, position.getZ() + 50));
+            List<Entity> entities = world.getOtherEntities(null, area);
+
+            for(Entity ent: entities){
+                if(ent instanceof LivingEntity){
+                    LivingEntity livingEntity = (LivingEntity) ent;
+                    livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 10, 0), null);
+                }
+            }
+        }
     }
 
     @Override
@@ -32,8 +62,12 @@ public class Sphinx extends AbstractIdol {
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (!world.isClient) {
+        if (!world.isClient && player.getStackInHand(hand).getItem().equals(Items.FLINT_AND_STEEL)) {
             world.setBlockState(pos, state.cycle(CHARGED), Block.NOTIFY_LISTENERS);
+            world.playSound(null, pos, SoundEvents.ENTITY_GENERIC_EXPLODE, SoundCategory.BLOCKS);
+            Vec3d dir = new Vec3d(player.getPos().x - pos.getX(), 2, player.getPos().z - pos.getZ()).normalize();
+            player.velocityModified = true;
+            player.setVelocity(dir);
         }
 
         return ActionResult.SUCCESS;
