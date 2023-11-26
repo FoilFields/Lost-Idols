d5ddef990fc6e8361df28489301f5d508b80dd1b
Lucas Eldon <lucas.eldon.2021@live.rhul.ac.uk>
2023-11-26 14:47:27 +0000

    Added promise idol sounds

--- src/main/java/foilfields/lostidols/idols/Promise.java
+++ src/main/java/foilfields/lostidols/idols/Promise.java
@@ -4,12 +4,29 @@ import foilfields.lostidols.init.Particles;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.entity.projectile.TridentEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+
+import static foilfields.lostidols.init.Sounds.PROMISE_CHARGE;
 
 public class Promise extends AbstractIdol {
     public Promise(Settings settings) {
@@ -22,6 +39,28 @@ public class Promise extends AbstractIdol {
             Vec3d particlePosition = position.toCenterPos();
             ((ServerWorld) world).spawnParticles(Particles.FIREFLY, particlePosition.getX(), particlePosition.getY(), particlePosition.getZ(), 1, 5, 2, 5, 0.01);
         }
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        if (!state.get(CHARGED) && itemStack.isOf(Items.FLINT_AND_STEEL)) {
+            Vec3d center = pos.toCenterPos();
+            world.playSound(player, center.getX(), center.getY(), center.getZ(), PROMISE_CHARGE, SoundCategory.BLOCKS, 1.0F, 1.0F);
+
+            itemStack.damage(1, player, (playerx) -> {
+                playerx.sendToolBreakStatus(hand);
+            });
+
+            if (!world.isClient()) {
+                world.setBlockState(pos, state.cycle(CHARGED), Block.NOTIFY_LISTENERS);
+                player.incrementStat(Stats.USED.getOrCreateStat(itemStack.getItem()));
+            }
+            
+            return ActionResult.success(world.isClient);
+        }
+
+        return ActionResult.PASS;
     }
 
     @Override
--- src/main/java/foilfields/lostidols/init/Sounds.java
+++ src/main/java/foilfields/lostidols/init/Sounds.java
@@ -9,9 +9,11 @@ import static foilfields.lostidols.LostIdols.GetIdentifier;
 public class Sounds {
     public static SoundEvent MOAI_SPIT_EVENT = SoundEvent.of(GetIdentifier("moai_spit"));
     public static SoundEvent MOAI_GROW_EVENT = SoundEvent.of(GetIdentifier("moai_grow"));
+    public static SoundEvent PROMISE_CHARGE = SoundEvent.of(GetIdentifier("promise_charge"));
 
     public static void init() {
         Registry.register(Registries.SOUND_EVENT, GetIdentifier("moai_spit"), MOAI_SPIT_EVENT);
         Registry.register(Registries.SOUND_EVENT, GetIdentifier("moai_grow"), MOAI_GROW_EVENT);
+        Registry.register(Registries.SOUND_EVENT, GetIdentifier("promise_charge"), PROMISE_CHARGE);
     }
 }
