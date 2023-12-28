600d822cffe31d3fdd0f6cf075a9ec807b54bdf8
Woukie <lucaseldon@live.com>
2023-12-28 20:52:00 +0000

    Tweaked some idols display properties. Removed jungle idol. Added magnet shrine structure

--- src/main/java/foilfields/lostidols/init/Blocks.java
+++ src/main/java/foilfields/lostidols/init/Blocks.java
@@ -30,7 +30,6 @@ public class Blocks {
     public static final Sphinx SPHINX_IDOL = new Sphinx(AbstractBlock.Settings.create().mapColor(MapColor.GOLD).instrument(Instrument.COW_BELL).requiresTool().strength(3.5F));
     public static final Moai MOAI_IDOL = new Moai(AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(Instrument.COW_BELL).requiresTool().strength(3.5F));
     public static final Bird BIRD_IDOL = new Bird(AbstractBlock.Settings.create().mapColor(MapColor.DEEPSLATE_GRAY).instrument(Instrument.COW_BELL).requiresTool().strength(3.5F));
-    public static final Jungle JUNGLE_IDOL = new Jungle(AbstractBlock.Settings.create().mapColor(MapColor.BLACK).instrument(Instrument.COW_BELL).requiresTool().strength(3.5F));
     public static final Promise PROMISE_IDOL = new Promise(AbstractBlock.Settings.create().mapColor(MapColor.LIGHT_GRAY).instrument(Instrument.COW_BELL).strength(0.3F).luminance((state) -> !state.get(Promise.CHARGED) ? 0 : 15).pistonBehavior(PistonBehavior.DESTROY).sounds(BlockSoundGroup.GLASS));
     public static final Undying UNDYING_IDOL = new Undying(AbstractBlock.Settings.create().mapColor(MapColor.GOLD).instrument(Instrument.COW_BELL).requiresTool().strength(3.0F, 6.0F).sounds(BlockSoundGroup.METAL).luminance((state) -> !state.get(Undying.CHARGED) ? 2 : 6));
     public static final Shulker SHULKER_IDOL = new Shulker(AbstractBlock.Settings.create().mapColor(MapColor.GOLD).instrument(Instrument.COW_BELL).requiresTool().strength(3.0F, 6.0F).sounds(BlockSoundGroup.METAL).luminance((state) -> !state.get(Undying.CHARGED) ? 0 : 3));
@@ -39,7 +38,7 @@ public class Blocks {
     public static final BlockEntityType<IdolBlockEntity> IDOL_BLOCK_ENTITY = Registry.register(
             Registries.BLOCK_ENTITY_TYPE,
             GetIdentifier("idol_block_entity"),
-            FabricBlockEntityTypeBuilder.create(IdolBlockEntity::new, SPHINX_IDOL, BIRD_IDOL, MOAI_IDOL, JUNGLE_IDOL, PROMISE_IDOL, UNDYING_IDOL, SHULKER_IDOL, ATTRACTION_IDOL, REPULSION_IDOL).build()
+            FabricBlockEntityTypeBuilder.create(IdolBlockEntity::new, SPHINX_IDOL, BIRD_IDOL, MOAI_IDOL, PROMISE_IDOL, UNDYING_IDOL, SHULKER_IDOL, ATTRACTION_IDOL, REPULSION_IDOL).build()
     );
 
     public static void init() {
@@ -51,7 +50,6 @@ public class Blocks {
         Registry.register(Registries.BLOCK, GetIdentifier("sphinx_idol"), SPHINX_IDOL);
         Registry.register(Registries.BLOCK, GetIdentifier("moai_idol"), MOAI_IDOL);
         Registry.register(Registries.BLOCK, GetIdentifier("bird_idol"), BIRD_IDOL);
-        Registry.register(Registries.BLOCK, GetIdentifier("jungle_idol"), JUNGLE_IDOL);
         Registry.register(Registries.BLOCK, GetIdentifier("promise_idol"), PROMISE_IDOL);
         Registry.register(Registries.BLOCK, GetIdentifier("undying_idol"), UNDYING_IDOL);
         Registry.register(Registries.BLOCK, GetIdentifier("shulker_idol"), SHULKER_IDOL);
@@ -61,7 +59,6 @@ public class Blocks {
         Registry.register(Registries.ITEM, GetIdentifier("sphinx_idol"), new BlockItem(SPHINX_IDOL, new FabricItemSettings()));
         Registry.register(Registries.ITEM, GetIdentifier("moai_idol"), new BlockItem(MOAI_IDOL, new FabricItemSettings()));
         Registry.register(Registries.ITEM, GetIdentifier("bird_idol"), new BlockItem(BIRD_IDOL, new FabricItemSettings()));
-        Registry.register(Registries.ITEM, GetIdentifier("jungle_idol"), new BlockItem(JUNGLE_IDOL, new FabricItemSettings()));
         Registry.register(Registries.ITEM, GetIdentifier("promise_idol"), new BlockItem(PROMISE_IDOL, new FabricItemSettings()));
         Registry.register(Registries.ITEM, GetIdentifier("undying_idol"), new BlockItem(UNDYING_IDOL, new FabricItemSettings()));
         Registry.register(Registries.ITEM, GetIdentifier("shulker_idol"), new BlockItem(SHULKER_IDOL, new FabricItemSettings()));
@@ -72,7 +69,6 @@ public class Blocks {
             entries.add(SPHINX_IDOL);
             entries.add(BIRD_IDOL);
             entries.add(MOAI_IDOL);
-            entries.add(JUNGLE_IDOL);
             entries.add(PROMISE_IDOL);
             entries.add(UNDYING_IDOL);
             entries.add(SHULKER_IDOL);
