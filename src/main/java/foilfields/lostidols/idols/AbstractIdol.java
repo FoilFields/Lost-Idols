69449e4730cad7ea4318f076ad23a7179c6a5d70
Jamie Markiewicz <jmarkiewicz88@outlook.com>
2024-03-07 16:31:13 +0000

    Added Dolphin Idol
    Made the idols waterloggable

--- src/main/java/foilfields/lostidols/idols/AbstractIdol.java
+++ src/main/java/foilfields/lostidols/idols/AbstractIdol.java
@@ -6,6 +6,8 @@ import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
@@ -17,6 +19,7 @@ import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 
 import static foilfields.lostidols.init.Blocks.IDOL_BLOCK_ENTITY;
@@ -24,11 +27,12 @@ import static foilfields.lostidols.init.Blocks.IDOL_BLOCK_ENTITY;
 public abstract class AbstractIdol extends BlockWithEntity implements BlockEntityProvider {
     public static final BooleanProperty CHARGED = Properties.ENABLED;
     public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 
     protected AbstractIdol(Settings settings) {
         super(settings);
 
-        this.setDefaultState(this.getDefaultState().with(CHARGED, false).with(FACING, Direction.NORTH));
+        this.setDefaultState(this.getDefaultState().with(CHARGED, false).with(FACING, Direction.NORTH).with(WATERLOGGED, false));
     }
 
     public abstract void tick(BlockState state, World world, BlockPos position);
@@ -56,12 +60,12 @@ public abstract class AbstractIdol extends BlockWithEntity implements BlockEntit
     @Override
     @Nullable
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(CHARGED, false).with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+        return this.getDefaultState().with(CHARGED, false).with(FACING, ctx.getHorizontalPlayerFacing().getOpposite()).with(WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).isOf(Fluids.WATER));
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(CHARGED, FACING);
+        builder.add(CHARGED, FACING, WATERLOGGED);
     }
 
     @Override
@@ -71,5 +75,20 @@ public abstract class AbstractIdol extends BlockWithEntity implements BlockEntit
 
     public BlockState rotate(BlockState state, BlockRotation rotation) {
         return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (state.get(WATERLOGGED)) {
+            // This is for 1.17 and below: world.getFluidTickScheduler().schedule(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
 }
